5b713be620291d2a7b4f95091fd980072c0b3d75
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-04-01 22:28:49 +0900

    bump to .net10

--- BTCPayServer.Plugins.Template/Plugin.cs
+++ BTCPayServer.Plugins.Template/Plugin.cs
@@ -10,7 +10,7 @@ public class Plugin : BaseBTCPayServerPlugin
 {
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     {
-        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=1.12.0" }
+        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=2.3.7" }
     };
 
     public override void Execute(IServiceCollection services)
